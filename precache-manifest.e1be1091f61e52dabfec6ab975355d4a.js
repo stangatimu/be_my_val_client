69037588fa282bb3c447d171d28d8c55df2a7d7e
stangatimu <stangatimu@gmail.com>
2020-02-11 19:41:01 +0300

    Updates

---
+++ precache-manifest.e1be1091f61e52dabfec6ab975355d4a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a279e0c65a4b61ea41a7852a5c88acc",
+    "url": "/be_my_val_client/index.html"
+  },
+  {
+    "revision": "2c072aa783f37d556513",
+    "url": "/be_my_val_client/static/css/main.891b5058.chunk.css"
+  },
+  {
+    "revision": "ec3e78bd0e94e1e6652c",
+    "url": "/be_my_val_client/static/js/2.8f1600b9.chunk.js"
+  },
+  {
+    "revision": "3bcc78cd5cbc517842848e779f665865",
+    "url": "/be_my_val_client/static/js/2.8f1600b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c072aa783f37d556513",
+    "url": "/be_my_val_client/static/js/main.7d254aae.chunk.js"
+  },
+  {
+    "revision": "d747d88c2e9cd24654ae",
+    "url": "/be_my_val_client/static/js/runtime-main.7000328f.js"
+  },
+  {
+    "revision": "af15c23de28551647b732c147a15b60b",
+    "url": "/be_my_val_client/static/media/bmv_bg.af15c23d.jpg"
+  },
+  {
+    "revision": "49522884d2bda247ea0d10fe59fdb9be",
+    "url": "/be_my_val_client/static/media/couple_kiss.49522884.gif"
+  },
+  {
+    "revision": "7b98e1eac5828ff90814d8d179686309",
+    "url": "/be_my_val_client/static/media/golden_ring.7b98e1ea.svg"
+  },
+  {
+    "revision": "383d1bd54b76e4a0c0a691dee319a06e",
+    "url": "/be_my_val_client/static/media/sad_cat.383d1bd5.gif"
+  }
+]);
